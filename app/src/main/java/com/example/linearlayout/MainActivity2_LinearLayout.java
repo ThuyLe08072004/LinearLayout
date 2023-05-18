5ba3f9b33dedda8ddf8e36923fd41846a14acaf0
ThuyLe08072004 <133005861+ThuyLe08072004@users.noreply.github.com>
2023-05-18 14:18:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/linearlayout/MainActivity2_LinearLayout.java
@@ -0,0 +1,14 @@
+package com.example.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2_LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_linear_layout);
+    }
+}
